1d09e335d8f9e66f61b918625139ce7df21d6b0d
Britta <britta.3.maier@uni-konstanz.de>
2025-07-31 12:44:08 +0200

    Add: Definition of variables

--- js-basics-and-variables/js-basics-and-variables_lifetime-calculator/index.js
+++ js-basics-and-variables/js-basics-and-variables_lifetime-calculator/index.js
@@ -20,3 +20,8 @@ Hint: You can combine text and variables using the `+` operator.
 const currentAge = Number(process.argv[2]); // This takes the `<age>` entered in the command `node index.js <age>`.
 
 console.log("Your current age is: " + currentAge);
+
+let numberDaysLived = Number(process.argv[2]) * 365;
+let numberDaysRemaining = (80 - Number(process.argv[2])) * 365;
+let percentageLived = (numberDaysLived / (80 * 365)) * 100;
+let numberDaysSleeping = numberDaysLived * 8;
