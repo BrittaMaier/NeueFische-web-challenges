02e4c88e3dceaebdf59f35ae7fec38385d3fdf66
Britta <britta.3.maier@uni-konstanz.de>
2025-08-11 12:24:31 +0200

    Add: Function declaration for logSeparator function

--- js-functions/js-functions_functions-with-parameters/js/index.js
+++ js-functions/js-functions_functions-with-parameters/js/index.js
@@ -60,5 +60,11 @@ Note: Separation text refers to the text that is logged in the console before ea
 */
 
 // --v-- write your code here --v--
-
+/*Version before solving the challenge 3 as an example for Exercise 1:
+console.log("-------------");
+console.log("Exercise 1:");*/
+/*My guess for the function:*/
+function logSeparator(numberExercise) {
+  console.log("-------------\nExercise " + numberExercise + ": ");
+}
 // --^-- write your code here --^--
