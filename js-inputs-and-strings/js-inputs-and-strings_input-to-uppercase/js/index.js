9623b7dc286fab338348b3fc452ab73b54387a00
Britta <britta.3.maier@uni-konstanz.de>
2025-08-12 10:16:33 +0200

    Fix: Challenge

--- js-inputs-and-strings/js-inputs-and-strings_input-to-uppercase/js/index.js
+++ js-inputs-and-strings/js-inputs-and-strings_input-to-uppercase/js/index.js
@@ -1,3 +1,8 @@
 console.clear();
-const firstInput = document.querySelector(["data-js='first-input'"]);
-const button = document.querySelector(["data-js='button-uppercase'"]);
+const firstInput = document.querySelector('[data-js="first-input"]');
+const button = document.querySelector('[data-js="button-uppercase"]');
+
+button.addEventListener("click", () => {
+  const value = firstInput.value;
+  firstInput.value = value.toUpperCase();
+});
