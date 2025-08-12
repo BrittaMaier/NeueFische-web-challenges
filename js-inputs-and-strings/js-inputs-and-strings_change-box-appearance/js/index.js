e9f6a74a73868a1b5a11a6d7a21a37a902dff302
Britta <britta.3.maier@uni-konstanz.de>
2025-08-12 11:09:19 +0200

    Add: Assignment of variables to input elements in js file

--- js-inputs-and-strings/js-inputs-and-strings_change-box-appearance/js/index.js
+++ js-inputs-and-strings/js-inputs-and-strings_change-box-appearance/js/index.js
@@ -1,1 +1,4 @@
 console.clear();
+const colorSlide = document.querySelector('[data-js="input-color"]');
+const radiusSlide = document.querySelector('[data-js="input-radius"]');
+const rotationSlide = document.querySelector('[data-js="input-rotation"]');
