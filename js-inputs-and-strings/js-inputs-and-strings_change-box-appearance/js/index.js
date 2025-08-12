85099f662053c61462069e92b28943b09a433f8c
Britta <britta.3.maier@uni-konstanz.de>
2025-08-12 11:35:39 +0200

    Add: Dynamic color change function to color Slide

--- js-inputs-and-strings/js-inputs-and-strings_change-box-appearance/js/index.js
+++ js-inputs-and-strings/js-inputs-and-strings_change-box-appearance/js/index.js
@@ -2,3 +2,8 @@ console.clear();
 const colorSlide = document.querySelector('[data-js="input-color"]');
 const radiusSlide = document.querySelector('[data-js="input-radius"]');
 const rotationSlide = document.querySelector('[data-js="input-rotation"]');
+const box = document.querySelector('[data-js="box"]');
+
+colorSlide.addEventListener("input", () => {
+  box.style.backgroundColor = `hsl(${colorSlide.value} 70% 60%)`;
+});
