613aabb6d86a2e343700739d540017e77803da51
Britta <britta.3.maier@uni-konstanz.de>
2025-08-12 10:35:26 +0200

    Assign: Variable to second input and apply event listener to switch button

--- js-inputs-and-strings/js-inputs-and-strings_switch-values/js/index.js
+++ js-inputs-and-strings/js-inputs-and-strings_switch-values/js/index.js
@@ -1,6 +1,7 @@
 console.clear();
 
 const firstInput = document.querySelector('[data-js="first-input"]');
+const secondInput = document.querySelector('[data-js="second-input"]');
 const uppercaseButton = document.querySelector('[data-js="button-uppercase"]');
 const lowercaseButton = document.querySelector('[data-js="button-lowercase"]');
 const switchButton = document.querySelector('[data-js="button-switch]');
@@ -11,4 +12,8 @@ uppercaseButton.addEventListener("click", () => {
 
 lowercaseButton.addEventListener("click", () => {
   firstInput.value = firstInput.value.toLowerCase();
+});
+
+switchButton.addEventListener("click", () => {
+  /**/
 });
