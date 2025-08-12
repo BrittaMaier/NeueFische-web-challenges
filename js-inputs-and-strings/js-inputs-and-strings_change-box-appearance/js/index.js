fc735582e3e8dc903f1adae6f2b756c27fb3d5f2
Britta <britta.3.maier@uni-konstanz.de>
2025-08-12 11:41:06 +0200

    Add: Dynmaic border radius and rotation change to respective slides

--- js-inputs-and-strings/js-inputs-and-strings_change-box-appearance/js/index.js
+++ js-inputs-and-strings/js-inputs-and-strings_change-box-appearance/js/index.js
@@ -7,3 +7,9 @@ const box = document.querySelector('[data-js="box"]');
 colorSlide.addEventListener("input", () => {
   box.style.backgroundColor = `hsl(${colorSlide.value} 70% 60%)`;
 });
+radiusSlide.addEventListener("input", () => {
+  box.style.borderRadius = `${radiusSlide.value}%`;
+});
+rotationSlide.addEventListener("input", () => {
+  box.style.transform = `rotate(${rotationSlide.value}deg)`;
+});
