b55fd4dd61159e17abd6e834663307e10ddb0953
Britta <britta.3.maier@uni-konstanz.de>
2025-08-13 08:53:15 +0200

    Add: Solution to first part analog to lecture plus focus on first name field

--- js-forms/js-forms_handle-form-submit/js/index.js
+++ js-forms/js-forms_handle-form-submit/js/index.js
@@ -1,3 +1,13 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener("click", (event) => {
+  event.preventDefault();
+  const formData = new FormData(event.targe);
+  const data = Object.fromEntries(formData);
+  event.target.reset();
+  const firstName = document.getElementById("first-name");
+  firstName.focus();
+  console.log(data);
+});
