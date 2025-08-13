8ff80172ae1ba615b2f0e173909bfcc5f815e568
Britta <britta.3.maier@uni-konstanz.de>
2025-08-13 08:58:38 +0200

    Fix:Spelling mistake

--- js-forms/js-forms_handle-form-submit/js/index.js
+++ js-forms/js-forms_handle-form-submit/js/index.js
@@ -4,7 +4,7 @@ const form = document.querySelector('[data-js="form"]');
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  const formData = new FormData(event.targe);
+  const formData = new FormData(event.target);
   const data = Object.fromEntries(formData);
   event.target.reset();
   const firstName = document.getElementById("first-name");
