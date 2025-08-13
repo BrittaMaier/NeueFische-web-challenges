9f6b454956ca0f5fce73dfa091cfe0ccc500fda3
Britta <britta.3.maier@uni-konstanz.de>
2025-08-13 09:19:49 +0200

    Add: Console log for Age-Badness-Sum

--- js-forms/js-forms_handle-form-submit/js/index.js
+++ js-forms/js-forms_handle-form-submit/js/index.js
@@ -6,6 +6,11 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const formData = new FormData(event.target);
   const data = Object.fromEntries(formData);
+  console.log(
+    `The age-badness-sum of ${event.target.firstName.value} is ${
+      Number(event.target.age.value) + Number(event.target.badness.value)
+    }`
+  );
   event.target.reset();
   const firstName = document.getElementById("first-name");
   firstName.focus();
