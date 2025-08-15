5d475c3d9e08a8d46e08bd8508255151bf6a878c
Britta <britta.3.maier@uni-konstanz.de>
2025-08-15 18:49:47 +0200

    Fix: NavKey is only string needs to be used as nav[navKey] to refer to value of the keys of nav

--- js-loops/js-loops_forin-select-nav/js/index.js
+++ js-loops/js-loops_forin-select-nav/js/index.js
@@ -38,16 +38,17 @@ const nav = {
 
 const navElement = document.createElement("nav");
 const ul = document.createElement("ul");
-main.append(navElement);
-navElement.append(ul);
 
 // --v-- write or modify code below this line --v--
 for (const navKey in nav) {
   let newLi = document.createElement("li");
   let newA = document.createElement("a");
-  newA.setAttribute("href", nav[navKey].href);
-  newA.textContent = nav[navKey];
-  newLi.textContent=newA;
-  ul.append(newLi);*/
+  newA.textContent = nav[navKey].text;
+  newA.href = nav[navKey].href;
+  newLi.appendChild(newA);
+  ul.appendChild(newLi);
 }
+main.append(navElement);
+navElement.append(ul);
+console.log(navElement);
 // --^-- write or modify code above this line --^--
