de060ff95b326f8b75279ce9607750a160a1bfea
Britta <britta.3.maier@uni-konstanz.de>
2025-08-15 15:49:59 +0200

    Add: for of

--- js-loops/js-loops_forof-list/js/index.js
+++ js-loops/js-loops_forof-list/js/index.js
@@ -14,7 +14,17 @@ const programmingLanguages = [
   "PHP",
   "Ruby",
 ];
+/*Your task is to create a list item for each array item and add it to the ordered list. Each list item should display the value of the current array item. Use a `for...of` loop to accomplish this.
+
+The following hints may guide you:
+
+- Do you remember the property for setting the text of an HTML element?
+- Be sure to append each list item to the ordered list.*/
 
 // --v-- write or modify code below this line --v--
-
+for (const elementArray of programmingLanguages) {
+  newLi = document.createElement("li");
+  newLi.textContent = elementArray;
+  ol.appendChild(newLi);
+}
 // --^-- write or modify code above this line --^--
