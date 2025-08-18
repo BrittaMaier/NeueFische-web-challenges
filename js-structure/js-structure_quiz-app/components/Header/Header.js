5e6858db5979342af9e9f42aa1120fc98843b912
Britta <britta.3.maier@uni-konstanz.de>
2025-08-18 19:26:40 +0200

    Transfer: Header part to header js

--- js-structure/js-structure_quiz-app/components/Header/Header.js
+++ js-structure/js-structure_quiz-app/components/Header/Header.js
@@ -2,8 +2,8 @@ export default function Header() {
   const header = document.createElement("header");
   header.classList.add("header");
   header.innerHTML = /* html */ `
-	  <h1 class="header__title">Quiz-App</h1>
-	`;
+    <h1 class="header__title">Quiz-App</h1>
+  `;
 
   return header;
 }
--- js-structure/js-structure_quiz-app/index.js
+++ js-structure/js-structure_quiz-app/index.js
@@ -1,9 +1,9 @@
 import Header from "./components/Header/Header.js";
 import Form from "./components/Form/Form.js";
-import CardList from "./components/CardList/CardList.js";
 import Card from "./components/Card/Card.js";
-import App from "./components/App/App.js";
 import Bookmark from "./components/Bookmark/Bookmark.js";
+import CardList from "./components/CardList/CardList.js";
+import App from "./components/App/App.js";
 const cards = [
   {
     question:
@@ -29,35 +29,11 @@ const cards = [
 ];
 
 const headerElement = Header();
-
-function handleFormSubmit(event) {
-  event.preventDefault();
-
-  const formData = new FormData(event.target);
-  const data = Object.fromEntries(formData);
-
-  const newCard = {
-    question: data.question,
-    answer: data.answer,
-    tags: data.tags.split(","),
-    isBookmarked: false,
-  };
-
-  cards.unshift(newCard);
-
-  // To prevent the need to rerender, we add the new card to the DOM directly
-  const cardList = document.querySelector(".card-list");
-  const newCardElement = Card(newCard);
-  cardList.prepend(newCardElement);
-
-  event.target.reset();
-  event.target.elements.question.focus();
-}
 const formElement = Form();
 const bookmarkElement = Bookmark();
 const cardElement = Card();
 const cardListElement = CardList();
-const appElement = App(headerElement, formElement, cardListElement);
+const appElement = App();
 // The render function renders the app to the DOM
 function render() {
   const root = document.querySelector("#root");
