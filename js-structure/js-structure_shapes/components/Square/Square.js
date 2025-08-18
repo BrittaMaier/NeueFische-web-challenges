13803e82f55575d64889be58928fe47662ff0732
Britta <britta.3.maier@uni-konstanz.de>
2025-08-18 15:38:02 +0200

    Add: Analog changes for square and pentagon

---
+++ js-structure/js-structure_shapes/components/Square/Square.js
@@ -0,0 +1,9 @@
+export default function Square() {
+  const square = document.createElement("div");
+  square.classList.add("square");
+  square.addEventListener("click", () => {
+    const randomColor = getRandomColor();
+    square.style.backgroundColor = randomColor;
+  });
+  return square;
+}
--- js-structure/js-structure_shapes/index.js
+++ js-structure/js-structure_shapes/index.js
@@ -1,22 +1,12 @@
 import getRandomColor from "./utils/randomColor.js";
 import Circle from "./components/Circle/Circle.js";
+import Square from "./components/Square/Square.js";
+import Pentagon from "./components/Pentagon/Pentagon.js";
 console.clear();
 
 const root = document.getElementById("root");
 const circleElement = Circle();
+const squareElement = Square();
+const pentagonElement = Pentagon();
 
-const square = document.createElement("div");
-square.classList.add("square");
-square.addEventListener("click", () => {
-  const randomColor = getRandomColor();
-  square.style.backgroundColor = randomColor;
-});
-
-const pentagon = document.createElement("div");
-pentagon.classList.add("pentagon");
-pentagon.addEventListener("click", () => {
-  const randomColor = getRandomColor();
-  pentagon.style.backgroundColor = randomColor;
-});
-
-root.append(circleElement, square, pentagon);
+root.append(circleElement, squareElement, pentagonElement);
