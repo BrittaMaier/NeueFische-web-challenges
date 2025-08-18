544ba4987cf669c735f6778549453b19df6b9f8d
Britta <britta.3.maier@uni-konstanz.de>
2025-08-18 16:36:16 +0200

    Add: Confusion

---
+++ js-structure/js-structure_quiz-app/components/CardList/CardList.js
@@ -0,0 +1,12 @@
+import Card from "../Card/Card.js";
+export default function CardList(Card) {
+  const cardList = document.createElement("section");
+  cardList.classList.add("card-list");
+
+  cards.forEach((card) => {
+    const cardElement = Card(card);
+    cardList.append(cardElement);
+  });
+
+  return cardList;
+}
