07219691ea9c150cc007420740f1975e094c7a2d
Britta <britta.3.maier@uni-konstanz.de>
2025-08-19 13:03:14 +0200

    Add: Function content

--- js-fetch/js-fetch_star-wars-console/js/index.js
+++ js-fetch/js-fetch_star-wars-console/js/index.js
@@ -2,6 +2,18 @@ console.clear();
 
 const url = "https://swapi.py4e.com/api/people";
 
-function fetchData() {}
+async function fetchData() {
+  const response = await fetch(url);
+  const data = await response.json();
+  if (!response.ok) {
+    console.error("Bad response, status code: ", response.status);
+  }
+  return data;
+}
 
-fetchData();
+const data = await fetchData();
+//const firstElementofArray = Data.results;
+console.log("Data: ", data);
+//console.log("Element of Data: ", firstElementofArray);
+console.log("Data results value: \n Array ", data.results);
+console.log("R2D2's eye-color: ", data.results[2]["eye_color"]);
