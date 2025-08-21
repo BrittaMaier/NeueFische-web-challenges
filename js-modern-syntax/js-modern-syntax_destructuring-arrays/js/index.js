b94d19a4fca1cfe0c28306e2fb9b3948df912f1f
Britta <britta.3.maier@uni-konstanz.de>
2025-08-21 13:13:47 +0200

    Add: Solutions to the other exercises

--- js-modern-syntax/js-modern-syntax_destructuring-arrays/js/index.js
+++ js-modern-syntax/js-modern-syntax_destructuring-arrays/js/index.js
@@ -16,6 +16,7 @@ Use array destructuring to extract the variables `firstNameOfChris`, `lastNameOf
 */
 
 const personChris = [12, "Chris", "Owen"];
+export const [ageOfChris, firstNameOfChris, lastNameOfChris] = personChris;
 
 /*
 EXERCISE 3
@@ -24,6 +25,7 @@ Ensure no unused variables remain.
 */
 
 const personAlex = ["Alex", 12, "Main"];
+export const [firstNameOfAlex, , lastNameOfAlex] = personAlex;
 
 /*
 EXERCISE 4
@@ -32,7 +34,7 @@ as variable called `lastName`.
 */
 
 const students = ["Christina", "Jon", "Alexandare"];
-
+export const [, , lastName] = students;
 /*
 EXERCISE 5
 Use array destructuring to extract all names from the nested array `nestedStudents`.
@@ -40,7 +42,10 @@ Assign each name to a variable called "student1" through "student5", in the give
 */
 
 const nestedStudents = ["Chris", ["Ahmad", "Antigoni"], ["Toby", "Sam"]];
-
+export const [student1, students23, students45] = nestedStudents;
+export const [student2, student3] = students23;
+export const [student4, student5] = students45;
+console.log(student1, student2, student3, student4, student5);
 /*
 EXERCISE 6
 Spread the values of the `values` array into the function call of the `add` function.
@@ -49,10 +54,11 @@ Keep in mind that you need to export the variable `result` to make the test work
 
 const values = [1, 6, 7, 9, 12, 5, 4];
 
-const result = add(1); // Spread values inside this function call
+export const result = add(...values); // Spread values inside this function call
 
 function add(...values) {
   return values.reduce(
     (previousValue, currentValue) => previousValue + currentValue
   );
 }
+console.log(result);
