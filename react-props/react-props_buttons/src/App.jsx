34bbee9710441ac43d19d368a194da4df0c35fb4
Britta <britta.3.maier@uni-konstanz.de>
2025-08-25 16:50:05 +0200

    Create: handleclick function inside Button component

--- react-props/react-props_buttons/src/App.jsx
+++ react-props/react-props_buttons/src/App.jsx
@@ -1,11 +1,15 @@
 export default function App() {
-  return <Button text="Hello" color="blue" disabled={true} />;
+  return <Button text="Hello" color="blue" disabled={false} />;
 }
 function Button({ text, color, disabled }) {
+  function handleClick() {
+    console.log("You clicked me");
+  }
   return (
     <button
       style={{ backgroundColor: color, border: "solid" }}
       disabled={disabled}
+      onClick={handleClick}
     >
       {text}
     </button>
