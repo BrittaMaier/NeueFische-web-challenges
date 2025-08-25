49361e1885c62a117dda7c714bb46c49d003f067
Britta <britta.3.maier@uni-konstanz.de>
2025-08-25 16:54:01 +0200

    Transfer: HandleClick function to app and parse it as a property to button

--- react-props/react-props_buttons/src/App.jsx
+++ react-props/react-props_buttons/src/App.jsx
@@ -1,15 +1,17 @@
 export default function App() {
-  return <Button text="Hello" color="blue" disabled={false} />;
+  return (
+    <Button text="Hello" color="blue" disabled={false} onClick={handleClick} />
+  );
 }
-function Button({ text, color, disabled }) {
-  function handleClick() {
-    console.log("You clicked me");
-  }
+function handleClick() {
+  console.log("You clicked me");
+}
+function Button({ text, color, disabled, onClick }) {
   return (
     <button
       style={{ backgroundColor: color, border: "solid" }}
       disabled={disabled}
-      onClick={handleClick}
+      onClick={onClick}
     >
       {text}
     </button>
