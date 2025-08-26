2d99d92352b59871d269b45bc5167faf1d91a6ae
Britta <britta.3.maier@uni-konstanz.de>
2025-08-26 14:57:40 +0200

    Add: State variable and useState

--- react-state-1/react-state-1_box/src/App.jsx
+++ react-state-1/react-state-1_box/src/App.jsx
@@ -1,18 +1,25 @@
 import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
-  let isActive = false;
-
+  const [isActive, setIsActive] = useState(false);
   function handleClick() {
-    isActive = !isActive;
-    // Check that the value changes correctly.
-    console.log(isActive);
+    setIsActive(!isActive);
   }
+  // Check that the value changes correctly.
+  console.log(isActive);
 
   return (
     <main>
       <div className={`box ${isActive ? "box--active" : ""}`} />
-      <button onClick={handleClick}>Activate</button>
+      <button onClick={handleClick}>
+        {isActive ? "Deactivate" : "Activate"}
+      </button>
     </main>
   );
 }
+/*Bonus:
+I think that is because before clicking the initial value of isActive is set to be false. Therefore, the first two 
+console logs log false to the console (first time for rendering the App component and second time for rendering the virtual
+react tree). When the button is clicked this firstly adresses the onClick event calling the
+handle click function starting the rendering of the App component from top to bottom?*/
