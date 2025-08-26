7af5b80011907a5caf1590fd9e2ce640de2b4a75
Britta <britta.3.maier@uni-konstanz.de>
2025-08-26 11:09:52 +0200

    Create: Link component with className and href and children as props

--- react-nesting/react-nesting_navigation/src/App.jsx
+++ react-nesting/react-nesting_navigation/src/App.jsx
@@ -3,25 +3,27 @@ import "./styles.css";
 import avatar from "./img/avatar.jpg";
 import logo from "./img/logo.jpg";
 import Header from "./components/Header";
+import Navigation from "./components/Navigation";
+import Link from "./components/Link";
 
 export default function App() {
   return (
     <>
       <Header className="header">
-        <a href="#">
+        <Link classname="" href="#">
           <img className="round-image" src={logo} alt="logo" />
-        </a>
-        <nav>
-          <a className="navigation__link" href="#home">
+        </Link>
+        <Navigation>
+          <Link className="navigation__link" href="#home">
             Home
-          </a>
-          <a className="navigation__link" href="#about">
+          </Link>
+          <Link className="navigation__link" href="#about">
             About
-          </a>
-          <a className="navigation__link" href="#impressum">
+          </Link>
+          <Link className="navigation__link" href="#impressum">
             Impressum
-          </a>
-        </nav>
+          </Link>
+        </Navigation>
         <button
           type="button"
           onClick={() => console.log("I could toggle a profile!")}
