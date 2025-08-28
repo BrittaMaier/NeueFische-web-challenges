be685f2d3bcc1d2ee605b2a6f9f6b959e8797a77
Britta <britta.3.maier@uni-konstanz.de>
2025-08-28 11:21:45 +0200

    Fix: Background color by removing curly brackets around color variable because already in curly brackets

--- react-with-arrays/react-with-arrays_fruits/src/components/Card.jsx
+++ react-with-arrays/react-with-arrays_fruits/src/components/Card.jsx
@@ -3,7 +3,7 @@ import "./Card.css";
 export default function Card({ key, name, color }) {
   console.log(key);
   return (
-    <p className="card" style={{ backgroundColor: { color } }}>
+    <p className="card" style={{ backgroundColor: color }}>
       {name}
     </p>
   );
