2d13bfaf8681d0541e396bd3e503ce26c42bbb55
Britta <britta.3.maier@uni-konstanz.de>
2025-08-28 16:29:54 +0200

    Add: Set variable and setter function as well as handle minus and handle plus function

--- react-state-2/react-state-2_colored-number/src/components/Counter.jsx
+++ react-state-2/react-state-2_colored-number/src/components/Counter.jsx
@@ -1,15 +1,24 @@
 import ColoredNumber from "./ColoredNumber";
+import { useState } from "react";
 
 export default function Counter() {
+  const [value, setValue] = useState(0);
+  function handleMinus() {
+    setValue((v) => v - 1);
+  }
+  function handlePlus() {
+    setValue((v) => v + 1);
+  }
   return (
     <div className="counter">
       <h1>Colored Number</h1>
-      <ColoredNumber value={0} />
+      <ColoredNumber value={value} />
       <div className="counter__buttons-container">
         <button
           type="button"
           className="counter__button"
           aria-label="increment count"
+          onClick={handlePlus}
         >
           +
         </button>
@@ -17,6 +26,7 @@ export default function Counter() {
           type="button"
           className="counter__button"
           aria-label="decrement count"
+          onClick={handleMinus}
         >
           -
         </button>
