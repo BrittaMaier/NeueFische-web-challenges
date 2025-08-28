99ba017d7dea016a7e537af49b5d3c8296c0bbab
Britta <britta.3.maier@uni-konstanz.de>
2025-08-28 13:25:37 +0200

    Add: checkAdmin function to tag.jsx

--- react-with-arrays/react-with-arrays_users/src/components/Tag.jsx
+++ react-with-arrays/react-with-arrays_users/src/components/Tag.jsx
@@ -1,5 +1,14 @@
 import "./Tag.css";
 
 export default function Tag({ tag }) {
+  //const isAdmin = tag.includes("admin");
+  return checkAdmin(tag);
+  //return <li className="tag">{tag}</li>;
+}
+
+function checkAdmin(tag) {
+  if (!tag.includes("admin")) {
+    return <li className="tag tag--highlight">{tag}</li>;
+  }
   return <li className="tag">{tag}</li>;
 }
