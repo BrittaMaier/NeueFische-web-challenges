8a838945969fdc8a68d74dfdb10554a35a4ac9b2
Britta <britta.3.maier@uni-konstanz.de>
2025-09-04 16:36:08 +0200

    Add: h1,introduction, h2, ul with li and link for each book and its corresponding page

--- nextjs-basics/lotr-app/pages/index.js
+++ nextjs-basics/lotr-app/pages/index.js
@@ -1,7 +1,23 @@
+import { introduction } from "@/data";
+import { volumes } from "@/data";
+
 export default function HomePage() {
   return (
     <div>
-      <h1>Hello from Next.js</h1>
+      <h1>Lord of the Rings</h1>
+      <p>{introduction}</p>
+      <h2>All Volumes</h2>
+      <ul>
+        <li>
+          <Link href={`/the-fellowship-of-the-ring`}>{volumes[0].title}</Link>
+        </li>
+        <li>
+          <Link href={`/the-return-of-the-king`}>{volumes[1].title} </Link>
+        </li>
+        <li>
+          <Link href={`/the-two-towers`}>{volumes[2].title} </Link>
+        </li>
+      </ul>
     </div>
   );
 }
