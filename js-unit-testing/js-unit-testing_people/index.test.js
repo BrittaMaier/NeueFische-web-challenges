653fa2ddcd292135f5e3df999b4167b8a89e4c5a
Britta <britta.3.maier@uni-konstanz.de>
2025-09-08 12:09:21 +0200

    Add: Implementation of getNameAndAge and fix Musterfrau in test

--- js-unit-testing/js-unit-testing_people/index.test.js
+++ js-unit-testing/js-unit-testing_people/index.test.js
@@ -59,18 +59,18 @@ test('getFullNames() returns an array with full names as strings, e.g. "Jane Doe
   ]);
 });
 
-/*test('getNameAndAge() returns an array of formatted strings like this: "Skywalker (23)"', () => {
+test('getNameAndAge() returns an array of formatted strings like this: "Skywalker (23)"', () => {
   const result = getNameAndAge(people);
   expect(result).toEqual([
     "Doe (21)",
     "Doe (18)",
     "Mustermann (32)",
-    "Mustermann (38)",
+    "Musterfrau (38)",
     "Skywalker (23)",
   ]);
 });
 
-test("getPeopleByAge() returns an array of people of a certain age", () => {
+/*test("getPeopleByAge() returns an array of people of a certain age", () => {
   const result = getPeopleByAge(people, 38);
   expect(result).toEqual([
     {
