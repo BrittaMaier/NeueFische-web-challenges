1e6641439c7b913920f9019c38da700f302bc423
Britta <britta.3.maier@uni-konstanz.de>
2025-09-16 12:20:37 +0200

    Add: server setup with random person and listen

--- backend-basics/backend-basics_chance/index.js
+++ backend-basics/backend-basics_chance/index.js
@@ -1,3 +1,6 @@
-// import { server } from "./server.js";
+import { server } from "./server.js";
 
-// server.listen(…
+const port = 8000;
+server.listen(port, "127.0.0.1", () => {
+  console.log("Server is running on localhost:" + port);
+});
--- backend-basics/backend-basics_chance/server.js
+++ backend-basics/backend-basics_chance/server.js
@@ -1,3 +1,16 @@
 import { createServer } from "node:http";
+import Chance from "chance";
 
-// export const server = …
+const chance = new Chance();
+
+export const server = createServer((req, res) => {
+  const person = {
+    name: chance.name(),
+    age: chance.age({ type: "adult" }),
+    profession: chance.profession({ rank: true }),
+  };
+  res.statusCode = 200;
+  res.end(
+    `Hello, my name is ${person.name} and I am ${person.age} years old. I am a ${person.profession}.`
+  );
+});
