714da9dc988921f49a5a72f2f89c51238196bde5
Britta <britta.3.maier@uni-konstanz.de>
2025-09-16 12:01:14 +0200

    Add: Server setup with conditions and listen

--- backend-basics/backend-basics_routes/index.js
+++ backend-basics/backend-basics_routes/index.js
@@ -1,3 +1,9 @@
 // import { server } from "./server.js";
 
 // server.listen(…
+import { server } from "./server.js";
+
+const port = 8000;
+server.listen(port, "127.0.0.1", () => {
+  console.log("Server is running on localhost:" + port);
+});
