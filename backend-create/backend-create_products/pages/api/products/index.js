6608183aa342ca59a010d7fd8c671ac864bf07e7
Britta <britta.3.maier@uni-konstanz.de>
2025-09-18 17:12:01 +0200

    Add: Post route to index api

--- backend-create/backend-create_products/pages/api/products/index.js
+++ backend-create/backend-create_products/pages/api/products/index.js
@@ -4,10 +4,28 @@ import Product from "@/db/models/Product";
 export default async function handler(request, response) {
   await dbConnect();
 
-  if (request.method === "GET") {
-    const products = await Product.find();
+  try {
+    //GET method means that we request to read data from the database
+    if (request.method === "GET") {
+      //here we refer to / copy every data from Product
+      const products = await Product.find();
+      //Here we define the statusCode property of the response object to have the value 200
+      //and add products (as an object) as a value to the body property of the response object
+      response.status(200).json(products);
 
-    response.status(200).json(products);
+      return;
+    }
+    //POST method means that I want to add new data to the database
+    if (request.method === "POST") {
+      console.log("In the POST");
+      const productData = request.body;
+      await Product.create(productData);
+
+      return response.status(201).json({ status: "Product created" });
+    }
+  } catch (error) {
+    console.error(error);
+    response.status(500).json({ message: "Internal Server Error." });
     return;
   }
 
