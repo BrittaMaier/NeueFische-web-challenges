c6d257310515624bf967ac7cfaea072c8c63f95e
Britta <britta.3.maier@uni-konstanz.de>
2025-09-19 12:26:18 +0200

    Add: Update and delete on back- and frontend side

--- backend-update-and-delete/backend-update-and-delete_products/pages/[id].js
+++ backend-update-and-delete/backend-update-and-delete_products/pages/[id].js
@@ -2,19 +2,50 @@ import useSWR from "swr";
 import styled from "styled-components";
 import { useRouter } from "next/router";
 import StyledLink from "@/components/Link";
+import ProductForm from "@/components/ProductForm";
+import { useState } from "react";
 
 export default function Product() {
+  const [showEditForm, setShowEditForm] = useState(false);
   const router = useRouter();
   const { id } = router.query;
 
-  const { data, isLoading } = useSWR(`/api/products/${id}`);
-
+  const { data, isLoading, mutate, error } = useSWR(`/api/products/${id}`);
+  console.log(error);
   if (isLoading) {
     return <h1>Loading...</h1>;
   }
 
   if (!data) {
     return;
+  }
+  async function handleEditProduct(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const productData = Object.fromEntries(formData);
+
+    const response = await fetch(`/api/products/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringigy(productData),
+    });
+    if (response.ok) {
+      console.log(response.json());
+      setShowEditForm(false);
+      mutate();
+    }
+  }
+
+  async function handleDeleteProduct() {
+    const response = await fetch(`/api/products/${id}`, {
+      method: "DELETE",
+    });
+    if (response.ok) {
+      router.push("/");
+    }
   }
 
   return (
@@ -24,6 +55,27 @@ export default function Product() {
       <p>
         Price: {data.price} {data.currency}
       </p>
+      <button
+        onClick={() => {
+          setShowEditForm(!showEditForm);
+        }}
+      >
+        <span role="img" aria-label="A pencil">
+          ✏️
+        </span>
+      </button>
+      <button onClick={handleDeleteProduct} disabled={showEditForm}>
+        <span role="img" aria-label="A cross indicating deletion">
+          ❌
+        </span>
+      </button>
+      {showEditForm && (
+        <ProductForm
+          onSubmit={handleEditProduct}
+          value={data.joke}
+          isEditMode={true}
+        />
+      )}
       <StyledLink href="/">Back to all</StyledLink>
     </ProductCard>
   );
